3901d93370c61991e88c8d13e6ca5ea5fd66428d
cassina <bernardo@cassina.space>
2016-02-27 17:47:10 -0600

    Push

--- web/blueprints/events.py
+++ web/blueprints/events.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from google.appengine.ext import ndb
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, redirect
 
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
@@ -9,22 +9,29 @@ from wtforms.validators import InputRequired
 
 events = Blueprint('events', __name__, template_folder='templates', url_prefix='/events')
 
+DEMO_URL = 'https://www.facebook.com/events/1315199878496490'
+
 
 @events.route('/home', methods=['GET', 'POST'])
 def events_home():
     form = RegisterEventForm()
     if form.validate_on_submit():
         event = EventModel()
-        event.fb_event_id = 'fake'
-        event.fb_event_url = 'fake'
-        event.owner = 'fake'
+        event.fb_id = form.fb_url.data.split('events/')[1]
+        event.url = form.fb_url.data
+        event.owner = 'fake-owner'
         event.put()
-        return 'Event created'
+        return redirect()
     return render_template('events_home.html', form=form)
 
 
+@events.route('/dashboard')
+def event_dashboard():
+    pass
+
+
 class RegisterEventForm(Form):
-    fb_event_url = StringField(description='Please paste de Facebook Event url Here',
+    fb_url = StringField(description='Please paste de Facebook Event url Here',
                                validators=[InputRequired()])
     submit = SubmitField()
 
@@ -32,9 +39,9 @@ class RegisterEventForm(Form):
 class EventModel(ndb.Model):
     created = ndb.DateTimeProperty(auto_now_add=True,
                                    indexed=True)
-    fb_event_url = ndb.StringProperty(required=True,
+    url = ndb.StringProperty(required=True,
                                       indexed=True)
-    fb_event_id = ndb.StringProperty(required=True,
+    fb_id = ndb.StringProperty(required=True,
                                      indexed=True)
     owner = ndb.StringProperty(required=True,
                                indexed=True)
