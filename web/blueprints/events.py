c09e0d325cc832c03c4558128c98f8f3ec06ba59
cassina <bernardo@cassina.space>
2016-02-28 00:07:02 -0600

    Fixed.

--- web/blueprints/events.py
+++ web/blueprints/events.py
@@ -59,7 +59,8 @@ def listen():
     # Queries Datastore every minute
     event_list = EventModel.query(ndb.AND(EventModel.event_time >= datetime.datetime.now(),
                                           EventModel.made_request == False)).fetch()
-    return json.dumps(event_list)
+    event_json = json(str(event_list))
+    return event_json
 
 
 @events.route('/notify/<event_id>/<user_id>')
