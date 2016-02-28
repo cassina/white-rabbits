062b661bcf3cabc2868720f3051bc4d7daee6463
cassina <bernardo@cassina.space>
2016-02-28 04:50:19 -0600

    Added template.

--- web/blueprints/events.py
+++ web/blueprints/events.py
@@ -68,10 +68,11 @@ def confirm_chelas():
         confirmation.put()
         return 'Thank you'
 
+
 @events.route('/dashboard/<event_id>')
 def dashboard(event_id):
     event = EventModel.query(EventModel.fb_event_id == event_id).get()
-    return render_template('events_home.html', event=event)
+    return render_template('event_dashboard.html', event=event)
 
 
 @events.route('/listen')
