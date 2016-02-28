bab7452a041fe87bb4254b35279c8f607f45b595
cassina <bernardo@cassina.space>
2016-02-27 18:13:36 -0600

    Added blueprint route

--- web/blueprints/canvas.py
+++ web/blueprints/canvas.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template, redirect
+from web.domain import RegisterEventForm
 
 FB_APP_URL = 'https://apps.facebook.com/952096844837567/'
 
 canvas = Blueprint('canvas', __name__, template_folder='templates', url_prefix='/')
 
+
 @canvas.route('/', methods=['GET'])
 def redirect_to_fb_app():
-  return redirect(FB_APP_URL, code=302)
+    return redirect(FB_APP_URL, code=302)
 
 
 @canvas.route('/', methods=['POST'])
 def canvas_home():
-    return render_template('index.html')
+    form = RegisterEventForm()
+    return render_template('index.html', form=form)
---
+++ web/domain.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from flask_wtf import Form
+from wtforms.fields import StringField, SubmitField, HiddenField
+from wtforms.validators import InputRequired
+from google.appengine.ext import ndb
+
+
+class RegisterEventForm(Form):
+    fb_url = StringField(description='Please paste de Facebook Event url Here',
+                         validators=[InputRequired()])
+    fb_user_id = HiddenField(validators=[InputRequired()])
+    submit = SubmitField()
+
+
+class EventModel(ndb.Model):
+    created = ndb.DateTimeProperty(auto_now_add=True,
+                                   indexed=True)
+    fb_event_id = ndb.StringProperty(required=True,
+                                     indexed=True)
+    fb_user_id = ndb.StringProperty(required=True,
+                                    indexed=True)
