d8112894bd2d05090b9e6014a9bf83aeba2a3f55
cassina <bernardo@cassina.space>
2016-02-27 18:52:57 -0600

    Added try to split.

--- web/blueprints/events.py
+++ web/blueprints/events.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, redirect, url_for, render_template, flash
-from google.appengine.ext import ndb
 
 from web.domain import RegisterEventForm, EventModel
 
@@ -12,12 +11,20 @@ events = Blueprint('events', __name__, template_folder='templates')
 def register():
     form = RegisterEventForm()
     if form.validate_on_submit():
-        e_id = form.fb_url.data.split('events/')[1]
+        # Try to strip the id value
+        try:
+            e_id = form.fb_url.data.split('events/')[1]
+        except ValueError:
+            flash('Lo sentimos, parece ser que la url '
+                  'que registraste no es una url de un '
+                  'evento de Facebook valida. Intenta nuevamente')
+            return redirect('canvas.home')
+
         event = EventModel()
         event.fb_event_id = e_id
         event.fb_user_id = form.fb_user_id.data
         event.fb_user_token = form.fb_user_token.data
-        new_key = event.put()
+        event.put()
         flash('Thank you for registering your Facebook event!')
         return redirect(url_for('events.dashboard', event_id=e_id))
 
