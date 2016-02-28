47e077240c9827e5173deaaeb3682c7d0fcf59de
Victor Borja <vborja@apache.org>
2016-02-28 01:27:02 -0600

    diff

--- web/blueprints/events.py
+++ web/blueprints/events.py
@@ -111,15 +111,12 @@ def attendants(event_id):
     response = get_attendants(event_id)
     event = EventModel.query(EventModel.fb_event_id == event_id).get()
     json_r = json.loads(response.text)
-    ids = [user['id'] for user in json_r['data']]
-    confirmations = DrinkConfirmationModel.query(DrinkConfirmationModel.fb_event_id == event_id).fetch()
-    confirmation_ids = [c.fb_user_id for c in confirmations]
-
-    pending_ids = set(ids).difference(set(confirmation_ids))
-
-    for id in pending_ids:
-        send_notification(event, user_id=id)
-
+    attending_ids = [user['id'] for user in json_r['data']]
+    already_sent = DrinkConfirmationModel.query(DrinkConfirmationModel.fb_event_id == event_id).fetch()
+    sent_user_ids = [c.fb_user_id for c in already_sent]
+    pending_ids = set(attending_ids) - set(sent_user_ids)
+    for user_id in pending_ids:
+        send_notification(event, user_id=user_id)
     return response.text
 
 
